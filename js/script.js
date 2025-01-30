353485cc81c6ac97c317790bfb356500a2247e7d
Nisa-droid-coder <nisashahnahar@gmail.com>
2025-01-30 18:24:54 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function register() {
+    alert("Redirecting to the registration page...");
+    // Here you can redirect to the registration page
+    window.location.href = "register.html";
+}
+
+function login() {
+    alert("Redirecting to the login page...");
+    // Here you can redirect to the login page
+    window.location.href = "login.html";
+}
+
+function logout() {
+    alert("You have been logged out.");
+    // Here you can handle logout logic
+    // For example, clear user session or redirect to home page
+}
